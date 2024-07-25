8293d55381b437c91645cd1e552ec940599c439e
Gonçalo Oliveira <goncalo.27@hotmail.com>
2024-07-25 14:53:43 +0100

    - Implemented a way for units to travel between cells; - Added visualization for the paths to travel; - Added more organiz movement with curves; - Implemented look roation so that units always look where they're going;

--- Assets/Scripts/Hex Map/HexGameUI.cs
+++ Assets/Scripts/Hex Map/HexGameUI.cs
@@ -62,7 +62,7 @@ public class HexGameUI : MonoBehaviour {
 
     void DoMove() {
         if (grid.HasPath) {
-            selectedUnit.Location = currentCell;
+            selectedUnit.Travel(grid.GetPath());
             grid.ClearPath();
         }
     }
